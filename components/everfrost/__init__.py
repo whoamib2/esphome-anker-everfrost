7401df4098cbc617e4cfc9b222aa5893081b3e3a
Whoamib <47765821+whoamib2@users.noreply.github.com>
2026-08-13 10:31:25 -0500

    Auto-load select support

--- components/everfrost/__init__.py
+++ components/everfrost/__init__.py
@@ -1,4 +1,4 @@
 CODEOWNERS = ["@whoamib2"]
 DEPENDENCIES = ["ble_client"]
-AUTO_LOAD = ["climate", "sensor", "binary_sensor", "button"]
+AUTO_LOAD = ["climate", "sensor", "binary_sensor", "button", "select"]
 MULTI_CONF = True
